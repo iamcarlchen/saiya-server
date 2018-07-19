ae5ac4410ae1239b6257544959ba951cde004bd3
xiaobc <527710279@qq.com>
2018-07-19 19:42:05 +0800

    用于freemarker模板 测试

---
+++ src/test/java/TplTest.java
@@ -0,0 +1,72 @@
+import com.greatbee.core.lego.LegoException;
+import com.greatbee.core.lego.util.LegoUtil;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * TplTest
+ *
+ * @author xiaobc
+ * @date 18/3/6
+ */
+public class TplTest {
+
+    /**
+     * 坑爹的 freemaker 模板 测试
+     * @param args
+     * @throws LegoException
+     */
+    public static void main(String[] args) throws LegoException {
+        Map<String,Object> map = new HashMap();
+        map.put("type","123,abc");
+        map.put("uuid",3333);
+        Map<String,Object> subMap = new HashMap<>();
+        subMap.put("city_code","3333");
+//        subMap.put("rp_number","");
+//        subMap.put("city_code","");
+        subMap.put("platform",null);
+        map.put("request",subMap);
+        String[] hostArray = {"longyan","dev","rs","com"};
+        map.put("hostArray",hostArray);
+
+        map.put("page","2");
+        map.put("pageSize","10");
+
+//        String tpl = "<#if (type?split(\",\")?size > 1) >${type?split(\",\")[1]}</#if>";
+
+        String tpl = "{uuid:${uuid},type:<#if (type?split(\",\")?size > 1) >${type?split(\",\")[1]}</#if>}";
+
+        String tpl2 = "<#if type?length == 0 >123<#else>type</#if>";
+
+        String tpl3 = "<#if request.city_code?length == 0 >9999999<#else>${request.city_code}</#if>";
+
+        //是否存在
+        String tpl4 = "<#if (request.city_code)?? >9999999<#else>${request.city_code}</#if>";
+
+        String tpl5 = "${(request.city_code)!'999999'}";
+
+        String tpl6 = "${(request.city_code=='')?string('999999',request.city_code)}";
+
+        //url模板转换
+        String tpl7 = "<#switch hostArray[1]><#case 'dev'>http://longyan.dev.rs.com<#break><#case 'uat1'>http://longyan.uat1.rs.com<#break><#case 'mklmall'>https://longyan.mklmall.com<#break><#case 'mmall'>https://longyan.mmall.com<#break></#switch>";
+
+
+        String tpl8 = "<#if (request.city_code)?? >'88888'<#else>${(request.city_code=='')?string('999999',request.city_code)}</#if>";
+
+        String tpl9="<#if (request.platform)?? >${(request.platform=='PC')?string('PC,H5','H5')}<#else>PC,H5</#if>";
+
+        String tpl10 = "${.now?string('yyMMddHHmmss')}";//yyMMddHHmmss
+
+        String tpl11 = "${.now?string('yyyy-MM-dd HH:mm:ss')}";//yyMMddHHmmss
+
+        String tpl12 = "${(((page!'1')?number - 1) * ((pageSize!'10')?number))?number}";
+
+        String result = LegoUtil.transferInputValue(tpl12, map);
+        System.out.println("result="+result);
+
+
+
+    }
+
+}
