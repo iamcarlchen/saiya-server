39411268822c6b12f693fe5b8957dc6861812021
xuechao.zhang <xuechao.zhang@chinaredstar.com>
2018-09-24 20:52:54 +0800

    微店对接

--- src/main/java/com/greatbee/xiaoyi/controller/XiaoYiAPIController.java
+++ src/main/java/com/greatbee/xiaoyi/controller/XiaoYiAPIController.java
@@ -1,6 +1,7 @@
 package com.greatbee.xiaoyi.controller;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.greatbee.base.bean.DBException;
 import com.greatbee.base.util.DataUtil;
@@ -74,24 +75,60 @@ public class XiaoYiAPIController extends TYController {
                 switch (type) {
                     case "weidian.order.non_payment":
                         //待付款（直接到账+担保交易）
+                        try {
+                            //创建订单
+                            String buyerMobilephone = contentObject.getJSONObject("message").getString("user_phone");
+                            Double amount = DataUtil.getDouble(contentObject.getJSONObject("message").getString("total"), 0);
+                            Integer count = DataUtil.getInt(contentObject.getJSONObject("message").getString("quantity"), 0);
+
+                            String createOrderQuery = "insert into `tb_order` (`name`,`description`,`createDate`,`buyerMobilephone`,`amount`,`orderStatus`,`orderStatusDesc`,`buyCount`,`serialNumber`,`partner`)";
+                            Object[] createOrderParams = new Object[]{"合伙人订单", "来自合伙人微店订单", DataUtil.formatDateTime(new Date(System.currentTimeMillis())), buyerMobilephone, amount, 1, "下单未付款", count, orderSN, ""};
+                            xiaoyiSQLAdapter.updateQuery(createOrderQuery, createOrderParams);
+                            //创建订单明细
+                            JSONArray itemJSONArray = contentObject.getJSONObject("message").getJSONArray("items");
+                            if (itemJSONArray != null && itemJSONArray.size() > 0) {
+                                for (int i = 0; i < itemJSONArray.size(); i++) {
+                                    JSONObject itemObject = itemJSONArray.getJSONObject(i);
+                                    String createOrderDetailQuery = "INSERT INTO `db_xiaoyi_partner`.`tb_order_detail` (`name`,`createDate`,`orderSerialNumber`,`count`,`price`,`itemSerialNumber`,`cover`) VALUES (?,?,?,?,?,?,?);";
+                                    Object[] createOrderDetailParams = new Object[]{
+                                            itemObject.getString("item_name"),
+                                            DataUtil.formatDateTime(new Date(System.currentTimeMillis())),
+                                            orderSN,
+                                            itemObject.getInteger("quantity"),
+                                            itemObject.getDouble("price"),
+                                            itemObject.getString("merchant_code"),
+                                            ""
+                                    };
+                                    xiaoyiSQLAdapter.updateQuery(createOrderDetailQuery, createOrderDetailParams);
+                                }
+                            }
+                        } catch (SQLException e) {
+                            e.printStackTrace();
+                        }
                         break;
                     case "weidian.order.already_payment":
                         //已付款（直接到账）/已付款待发货（担保交易）
+
                         break;
                     case "weidian.order.delivered":
                         //已发货（担保交易）/已完成（直接到账+货到付款）
+
                         break;
                     case "weidian.order.confirm_eceipt":
                         //已确认收货（担保交易）
+
                         break;
                     case "weidian.order.finished":
                         //交易成功
+
                         break;
                     case "weidian.order.refund_byweidian":
                         //微店退款中
+
                         break;
                     case "weidian.order.refund_suc":
                         //退款成功
+
                         break;
                 }
             }
