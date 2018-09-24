a949011e3ec6ab76aeb58e203ab5cfd55636afb8
xuechao.zhang <xuechao.zhang@chinaredstar.com>
2018-09-24 17:15:01 +0800

    微店对接

---
+++ src/main/java/com/greatbee/xiaoyi/bean/WeidianResponse.java
@@ -0,0 +1,13 @@
+package com.greatbee.xiaoyi.bean;
+
+public class WeidianResponse {
+    private String status="success";
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
---
+++ src/main/java/com/greatbee/xiaoyi/controller/XiaoYiAPIController.java
@@ -0,0 +1,35 @@
+package com.greatbee.xiaoyi.controller;
+
+import com.alibaba.fastjson.JSONObject;
+import com.greatbee.procut.Response;
+import com.greatbee.procut.TYController;
+import com.greatbee.xiaoyi.bean.WeidianResponse;
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@Controller
+@RequestMapping("/xiaoyi")
+public class XiaoYiAPIController extends TYController {
+    private static final Logger logger = Logger.getLogger(com.greatbee.product.controller.Index.class);
+
+    /**
+     * 用于接收微店推送的订单消息
+     *
+     * @param request
+     * @param response
+     * @return
+     */
+    @RequestMapping(value = "/weidian/message", method = {RequestMethod.POST, RequestMethod.GET}, produces = "application/json;charset=UTF-8")
+    @ResponseBody
+    WeidianResponse weidianMessage(HttpServletRequest request, HttpServletResponse response, @RequestBody JSONObject jsonParam) {
+        logger.info("[weidian][message_push]");
+        logger.info("[weidian][message_push_body]" + jsonParam.toJSONString());
+        logger.info("[weidian][message_push_end]");
+        return new WeidianResponse();
+    }
+}
